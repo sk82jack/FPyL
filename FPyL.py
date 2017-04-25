1c0eff30b904a17a695b80110d78132ade4569b4
sk82jack <sk82jack@hotmail.com>
2017-04-25 19:40:16 +0100

    Re-write
    Converted to pythonic code and removed dependency for urllib so the
    scripts can be used in Python3

--- FPyL.py
+++ FPyL.py
@@ -1,81 +1,82 @@
-import json
-import urllib
+from concurrent.futures import ThreadPoolExecutor, as_completed
 import csv
 import requests
-from concurrent.futures import ThreadPoolExecutor, as_completed
 
-#gets current gameweek number
-def GetCurrentGameweek():
+def get_current_gameweek():
+    """Displays the current gameweek number"""
     url = "https://fantasy.premierleague.com/drf/events/"
-    response = urllib.urlopen(url)
-    gameweek = json.loads(response.read())
-    for weeks in range(len(gameweek)):
-        while gameweek[weeks]["is_current"]:
-            return gameweek[weeks]["id"]
-            break
+    response = requests.get(url)
+    json = response.json()
+    for gameweek in reversed(range(len(json))):
+        while json[gameweek]["is_current"]:
+            return int(json[gameweek]["id"])
 
-#gets count of current active FPL players
-def GetPlayerCount():
+def get_player_count():
+    """Displays the total number of Fantasy Premier League players"""
     url = "https://fantasy.premierleague.com/drf/elements/"
-    response = urllib.urlopen(url)
-    players = json.loads(response.read())
-    return len(players)
-    
-#creates a requests session which logs you into the FPL website. use for any code where you need to log in to retreieve data
-def FPLlogin():
-    with requests.Session() as session:
-            values = {'csrfmiddlewaretoken': '##your token here##',
-                      'login': '##fpl login email##',
-                      'password': '##fpl login password##',
-                      'app': 'plfpl-web',
-                      'redirect_uri': 'https://fantasy.premierleague.com/a/login'}
-            session.post('https://users.premierleague.com/accounts/login/',data = values)
-    return session
-   
-#creates JSON object of all player details with total scores,teams,positions etc.
-def CreatePlayerList():
-    response = urllib.urlopen("https://fantasy.premierleague.com/drf/elements/")
-    PlayerList = json.loads(response.read(),)
-    return PlayerList
+    response = requests.get(url)
+    json = response.json()
+    return int(len(json))
 
-#Creates JSON object containing team names with ID numbers for matching data
-def GetTeams():
-    response = urllib.urlopen('https://fantasy.premierleague.com/drf/teams/')
-    Teams = json.loads(response.read(),)
-    MyTeams = []
+def fpl_login(username, password):
+    """Creates a requests session which logs you into the FPL website."""
+    with requests.Session() as fpl_session:
+        values = {'csrfmiddlewaretoken': '##your token here##',
+                  'login': username,
+                  'password': password,
+                  'app': 'plfpl-web',
+                  'redirect_uri': 'https://fantasy.premierleague.com/a/login'}
+        fpl_session.post('https://users.premierleague.com/accounts/login/', data=values)
+    return fpl_session
 
-    for i in range(len(Teams)):
-        TeamEdit = {}
-        TeamEdit['Name'] = Teams[i].get('name')
-        TeamEdit['ID'] = Teams[i].get('id')
-        MyTeams.append(TeamEdit)
-    return MyTeams
+def create_player_list():
+    """creates JSON object of all player details with total scores,teams,positions etc."""
+    url = "https://fantasy.premierleague.com/drf/elements/"
+    response = requests.get(url)
+    json = response.json()
+    return json
 
+def get_teams():
+    """Creates JSON object containing team names with ID numbers for matching data"""
+    url = "https://fantasy.premierleague.com/drf/teams/"
+    response = requests.get(url)
+    json = response.json()
+    my_teams = []
 
-#creates CSV file containing all data from the current gameweek
-def GetGameweekData():    
-    gw = GetCurrentGameweek() 
-    MyWeek = []      
-    urls = ["https://fantasy.premierleague.com/drf/element-summary/" + str(i + 1) for i in range(GetPlayerCount())]
+    for i in range(len(json)):
+        team = {}
+        team['Name'] = json[i].get('name')
+        team['ID'] = json[i].get('id')
+        my_teams.append(team)
+    return my_teams
+
+def get_gameweek_data():
+    """Creates CSV file containing all data from the current gameweek"""
+    gameweek = get_current_gameweek()
+    my_week = []
+    urls = []
+    futures = []
+    for i in range(get_player_count()):
+        urls.append("https://fantasy.premierleague.com/drf/element-summary/" + str(i + 1))
     pool = ThreadPoolExecutor(len(urls))
-    futures = [pool.submit(urllib.urlopen,url) for url in urls]
+    for url in urls:
+        futures.append(pool.submit(urllib.urlopen, url))
     results = [r.result() for r in as_completed(futures)]
     for r in results:
         player = json.loads(r.read())
-        
-        if player['history'][0]['round'] >  gw:
-                break
+        if player['history'][0]['round'] > gameweek:
+            break
 
         for weeks in player['history']:
-            if weeks['round'] == gw:
-                MyWeek.append(weeks)
-    
-    Filename ='##your filepath##\Week' + str(gw) + '.csv'
+            if weeks['round'] == gameweek:
+                my_week.append(weeks)
+
+    Filename = '##your filepath##\Week' + str(gameweek) + '.csv'
 
     ThisWeek = open(Filename, 'w')
-    csvwriter = csv.writer(ThisWeek,lineterminator = '\n')
+    csvwriter = csv.writer(ThisWeek, lineterminator = '\n')
     count = 0
-    for players in MyWeek:
+    for players in my_week:
 
         if count == 0:
             header = players.keys()
@@ -84,5 +85,3 @@ def GetGameweekData():
         csvwriter.writerow(players.values())
 
     ThisWeek.close()
-
-    
