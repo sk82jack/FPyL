db18ae093cebd7f5ad64bd066f734471f90e6d83
sk82jack <sk82jack@hotmail.com>
2017-05-02 01:46:04 +0100

    Add MyFixtures.py
    Add MyFixtures.py

---
+++ Myfixtures.py
@@ -0,0 +1,64 @@
+import argparse
+import colorama
+import FPyL
+import terminaltables
+
+colorama.init()
+
+parser = argparse.ArgumentParser()
+parser.add_argument('email', help='Enter your FPL email address here')
+parser.add_argument('password', help='Enter your FPL password here')
+args = parser.parse_args()
+
+result = []
+fpl_session = FPyL.fpl_login(args.email, args.password)
+
+# Get users team info
+response = FPyL.json_response(url='https://fantasy.premierleague.com/drf/transfers', session=fpl_session)
+fpl_session.close()
+player_ids = [player['element'] for player in response['picks']]
+for player_id in player_ids:
+    # Get player names from the IDs
+    for player in FPyL.json_response('https://fantasy.premierleague.com/drf/elements/'):
+        if player['id'] == player_id:
+            name = player['web_name']
+    # Create the player dictionary
+    temp_player = {
+        'id': player_id,
+        'name': name
+    }
+    # Get future fixtures info
+    fixtures = FPyL.json_response('https://fantasy.premierleague.com/drf/element-summary/' + str(player_id))['fixtures']
+    for fixture in fixtures:
+        if fixture['difficulty'] <= 2:
+            opponent = colorama.Fore.GREEN + fixture['opponent_name'] + colorama.Style.RESET_ALL
+        elif fixture['difficulty'] <= 3:
+            opponent = colorama.Fore.YELLOW + fixture['opponent_name'] + colorama.Style.RESET_ALL
+        else:
+            opponent = colorama.Fore.RED + fixture['opponent_name'] + colorama.Style.RESET_ALL
+        if fixture['event_name'] not in temp_player:
+            temp_player[fixture['event_name']] = opponent
+        else:
+            temp_player[fixture['event_name']] = temp_player[fixture['event_name']] + '\n' + opponent
+    result.append(temp_player)
+headers = ['Player']
+# Get the upcoming gameweek
+gw = response['entry']['current_event'] + 1
+max_gw = gw + 5
+while (gw <= 38) and (gw <= max_gw):
+    headers.append('Gameweek ' + str(gw))
+    gw += 1
+data = []
+data.append(headers)
+for player in result:
+    row = []
+    row.append(player['name'])
+    for gameweek in headers[1:]:
+        row.append(player[gameweek])
+    data.append(row)
+
+table = terminaltables.SingleTable(data, title=colorama.Back.WHITE + colorama.Fore.BLACK + 'Upcoming Fixtures' + colorama.Style.RESET_ALL)
+table.inner_row_border = True
+for i in range(len(headers)):
+    table.justify_columns[i] = 'center'
+print(table.table)
